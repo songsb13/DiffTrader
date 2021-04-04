0ded09e7d3f4008a5d112d76270691ecf02457e0
gimo <goodmoskito@gmail.com>
2021-04-04 15:19:48 +0900

    add profit_thread, divide

---
+++ apps/trading/threads/profit_thread.py
@@ -0,0 +1,26 @@
+from Util.pyinstaller_patch import *
+from datetime import datetime
+from PyQt5.QtCore import QThread
+from DiffTrader.apps.trading.apis import get_expected_profit_by_server
+
+
+class TopProfitThread(QThread):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+
+    def run(self):
+        while True:
+            try:
+                date_row = get_expected_profit_by_server()
+                self.update_table(date_row)
+            except:
+                debugger.exception("Top Profit Thread ERROR")
+                time.sleep(60)
+
+    def update_table(self, rows):
+        self.parent.top_profit_model._data = rows
+        self.parent.top_profit_model.dataChanged.emit(self.parent.top_profit_model.createIndex(0, 0),
+                                                      self.parent.top_profit_model.createIndex(
+                                                          self.parent.top_profit_model.rowCount(),
+                                                          self.parent.top_profit_model.columnCount()))
