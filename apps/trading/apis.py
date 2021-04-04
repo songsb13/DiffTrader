19a6facd97fb8ec741082462c68530d4eedcbb40
gimo <goodmoskito@gmail.com>
2021-04-04 15:19:59 +0900

    api fixed

--- apps/trading/apis.py
+++ apps/trading/apis.py
@@ -1,36 +1,20 @@
 from Util.pyinstaller_patch import *
 from datetime import datetime
-from PyQt5.QtCore import QThread
 
 
-class TopProfitThread(QThread):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
+def get_expected_profit_by_server():
+    profit_path = 'http://saiblockchain.com/api/expected_profit'
+    now_date = time.time()
+    yesterday = now_date - 24 * 60 * 60
 
-    def run(self):
-        while True:
-            try:
-                start = time.time()
-                yesterday = start - 24 * 60 * 60
-                r = requests.get('http://saiblockchain.com/api/expected_profit',
-                                 json={'from': yesterday, 'to': start}
-                                 )
-                data = r.json()
-                if data:
-                    for dt in data:
-                        dt[-1] = datetime.fromtimestamp(dt[-1]).strftime(
-                            '%Y{} %m{} %d{} %H{} %M{}').format('년', '월', '일', '시', '분')
-                    self.update_table(data)
-                time.sleep(60)
-            except:
-                debugger.exception("Top Profit Thread ERROR")
-                time.sleep(60)
+    rq = requests.get(profit_path, json={'from': yesterday, 'to': now_date})
+    result = rq.json()
+    if result:
+        for date_ in result:
+            profit_date = datetime.fromtimestamp(date_[-1]).strftime(
+                '%Y{} %m{} %d{} %H{} %M{}').format('년', '월', '일', '시', '분')
+            date_[-1] = profit_date
 
-    def update_table(self, rows):
-        self.parent.top_profit_model._data = rows
-        self.parent.top_profit_model.dataChanged.emit(self.parent.top_profit_model.createIndex(0, 0),
-                                                      self.parent.top_profit_model.createIndex(
-                                                          self.parent.top_profit_model.rowCount(),
-                                                          self.parent.top_profit_model.columnCount()))
+        return date_
+
 
