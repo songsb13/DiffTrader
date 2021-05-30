9277c1479e68230fc00ffb4603f23668224089ff
gimo <goodmoskito@gmail.com>
2021-05-30 15:18:55 +0900

    mock file

---
+++ trading/mockup.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import MagicMock, Mock
+
+
+def profit_table_mock():
+    """
+    trade_date,
+    symbol,
+    primary_exchange,
+    secondary_exchange,
+    profit_btc,
+    profit_percent
+    """
+    return [[1622354947, 'BTC_XRP', 'binance', 'bithumb', 0.001, 0.1],
+            [1622350000, 'BTC_ETH', 'upbit', 'bithumb', 0.005, 0.2],
+            [1622340000, 'BTC_EOS', 'upbit', 'binance', 0.007, 0.3]]
+
+
+def profit_setting_mock():
+    return {
+        'min_profit_percent': 0.3,
+        'min_profit_btc': 0.0075
+    }
