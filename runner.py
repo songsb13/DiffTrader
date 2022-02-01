8fd7441d589671711d9af9233edb9b52aa40af20
gimo <goodmoskito@gmail.com>
2022-02-01 15:19:07 +0900

    fix runner

--- runner.py
+++ runner.py
@@ -1,24 +1,33 @@
-from concurrent.futures import ProcessPoolExecutor
-
 from DiffTrader.Setter.setter import Setter
 from DiffTrader.Monitoring.monitoring import Monitoring
-from DiffTrader.Trading.trading import Trading
 
 from DiffTrader.Util.utils import get_exchanges
-from DiffTrader.GlobalSetting.settings import AVAILABLE_EXCHANGES, TEST_USER
+from DiffTrader.GlobalSetting.settings import TEST_USER
+
+import time
 
 
 def run_setter():
-    pass
+    for exchange_str in exchanges.keys():
+        setter = Setter(TEST_USER, exchange_str)
+        setter.start()
 
 
 def run_monitoring():
-    pass
+    available = list(exchanges.keys())
+    for n, primary in enumerate(available):
+        for secondary in list(available)[n+1:]:
+            monitoring = Monitoring(TEST_USER, primary, secondary)
+            monitoring.start()
 
 
-def run_trading():
-    pass
+def run():
+    run_setter()
+    run_monitoring()
 
 
 if __name__ == '__main__':
     exchanges = get_exchanges()
+    run()
+
+    time.sleep(6000)
