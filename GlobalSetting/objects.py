241284ffc524dfe54058d1f1603e31f57cdbc5fb
gimo <goodmoskito@gmail.com>
2022-04-23 15:39:47 +0900

    add BaseProcess

--- GlobalSetting/objects.py
+++ GlobalSetting/objects.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from multiprocessing import Process
 from DiffTrader.GlobalSetting.settings import (
@@ -7,14 +8,17 @@ from DiffTrader.GlobalSetting.settings import (
 from DiffTrader.Util.utils import (
     get_exchanges,
     set_redis,
-    get_redis
+    publish_redis,
+    get_redis,
+    DecimalDecoder
 )
 
 from Util.pyinstaller_patch import debugger
 
 
 class BaseAPIProcess(Process):
-    api_redis_key = ''
+    pub_api_redis_key = ''
+    sub_api_redis_key = ''
 
     def __init__(self, exchange_str):
         super(BaseAPIProcess, self).__init__()
@@ -27,6 +31,7 @@ class BaseAPIProcess(Process):
         exchange = exchanges[self._exchange_str]
 
         after_time = time.time() + self._wait_time
+        lazy_cache = {}
         while True:
             """
             priority = api priority
@@ -34,17 +39,21 @@ class BaseAPIProcess(Process):
             name: api function name
             args: api args
             kwargs: api kwargs
+            get data from pub_api_redis_key and send to sub_api_redis_key
+            2개의 함수 그룹
+            async로 돌아가야할 그룹, sync한 그룹
+            deposit_addrs랑 get_transaction_fee
             """
-            info = get_redis(self.api_redis_key)
-
+            info = get_redis(self.pub_api_redis_key)
             if not info:
                 continue
+            if info['is_lazy'] and info['fn_name'] in lazy_cache:
+                set_redis(self.sub_api_redis_key, lazy_cache[info['fn_name']])
 
-            function_ = getattr(exchange, info['name'])
+            function_ = getattr(exchange, info['fn_name'])
 
             self._api_container[info['priority']].add((function_, info['args'], info['kwargs']))
             if time.time() >= after_time:
-                sorted(self._api_container, key=lambda priority: priority[-1])
                 for fn, args, kwargs, _ in self._api_container:
                     result = fn(*args, **kwargs)
 
@@ -52,13 +61,53 @@ class BaseAPIProcess(Process):
                         debugger.debug(result.message)
                         # set log
                     data = {
-                        info['name']: {
-                            'receive_type': info['receive_type'],
-                            'success': result.success,
-                            'data': result.data,
-                            'message': result.message
+                        info['receive_type']: {
+                            info['fn_name']: {
+                                'success': result.success,
+                                'data': result.data,
+                                'message': result.message
+                            }
                         }
                     }
-                    set_redis(self.api_redis_key, data)
+                    set_redis(self.sub_api_redis_key, data)
 
 
+class BaseProcess(object):
+    # base process for setter, monitoring and etc..
+    pub_api_redis_key = ''
+    sub_api_redis_key = ''
+    receive_type = ''
+    require_functions = []
+
+    def unpacking_message(self, api_contents):
+        if api_contents:
+            raw_data = api_contents.get('data', 1)
+            if isinstance(raw_data, int):
+                return None
+
+            to_json = json.loads(raw_data, cls=DecimalDecoder)
+            if not to_json:
+                return None
+
+            data = to_json.get(self.receive_type, None)
+            if data is None:
+                return None
+
+            result = {}
+            for key in data.keys():
+                if key not in self.require_functions:
+                    continue
+                result[key] = data[key]
+            else:
+                return result
+
+    def pub_api_fn(self, fn_name, is_async=False, is_lazy=False, api_priority=APIPriority.SEARCH):
+        publish_redis(self.pub_api_redis_key, {
+            'is_async': is_async,
+            'is_lazy': is_lazy,
+            'receive_type': self.receive_type,
+            'fn_name': fn_name,
+            'args': [],
+            'kwargs': {},
+            'priority': api_priority,
+        })
