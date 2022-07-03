e7b17c581e2d9a7d7ae6066e536d26efda1a3a79
gimo <goodmoskito@gmail.com>
2022-07-03 14:47:48 +0900

    api_process.py, error fixed.

--- Util/api_process.py
+++ Util/api_process.py
@@ -4,19 +4,20 @@
 """
 
 
-from DiffTrader.GlobalSetting.settings import RedisKey
-import time
-
 from multiprocessing import Process
+import time
+import json
+
 from DiffTrader.GlobalSetting.settings import (
     APIPriority,
-    TraderConsts
+    TraderConsts,
+    RedisKey
 )
 from DiffTrader.Util.utils import (
     get_exchanges,
     subscribe_redis,
-    get_redis,
-    publish_redis
+    publish_redis,
+    DecimalDecoder
 )
 
 from Util.pyinstaller_patch import debugger
@@ -30,7 +31,7 @@ class BaseAPIProcess(Process):
         super(BaseAPIProcess, self).__init__()
         self._exchange_str = exchange_str
         self._wait_time = 3
-        self._api_container = [set() for _ in range(APIPriority.LENGTH)]
+        self._api_container = [[] for _ in range(APIPriority.LENGTH)]
         self._api_subscriber = subscribe_redis(self.pub_api_redis_key)
 
     def run(self) -> None:
@@ -45,46 +46,62 @@ class BaseAPIProcess(Process):
                 결과 값을 전체 도메인에 broadcast하고, 결과 값 receive_type을 통해 각 도메인에서 데이터 판단을 진행한다.
                 현재 사용 도메인: setter, withdrawal
             """
-            info = self._api_subscriber.get_message().get('data', 1)
+            message = self._api_subscriber.get_message()
+            if message:
+                info = message.get('data', 1)
+            else:
+                info = message
             if not info or isinstance(info, int):
+                time.sleep(1)
                 continue
-            if not (time.time() <= refresh_time) and info['is_lazy'] and info['fn_name'] in lazy_cache:
+            info = json.loads(info, cls=DecimalDecoder)
+            if time.time() > refresh_time and info['is_lazy'] and info['fn_name'] in lazy_cache:
+                refresh_time = time.time() + TraderConsts.DEFAULT_REFRESH_TIME
                 publish_redis(self.sub_api_redis_key, lazy_cache[info['receive_type']][info['fn_name']])
 
             function_ = getattr(exchange, info['fn_name'])
 
-            self._api_container[info['priority']].add((function_, info['args'], info['kwargs']))
+            self._api_container[int(info['priority'])].append((function_, info['args'], info['kwargs']))
             if time.time() >= after_time:
-                for fn, args, kwargs, _ in self._api_container:
-                    result = fn(*args, **kwargs)
-
-                    if not result.success:
-                        debugger.debug(result.message)
-                        # set log
-                    data = {
-                        info['receive_type']: {
-                            info['fn_name']: {
-                                'success': result.success,
-                                'data': result.data,
-                                'message': result.message
+                for container in self._api_container:
+                    if not container:
+                        continue
+                    for fn, args, kwargs, *_ in container:
+                        # corutine에 대한 처리 필요함.
+                        result = fn(*args, **kwargs)
+                        if not result.success:
+                            debugger.debug(result.message)
+                            # set log
+                        data = {
+                            info['receive_type']: {
+                                info['fn_name']: {
+                                    'success': result.success,
+                                    'data': result.data,
+                                    'message': result.message
+                                }
                             }
                         }
-                    }
-                    lazy_cache.update(data)
-                    publish_redis(self.sub_api_redis_key, data)
+                        lazy_cache.update(data)
+                        publish_redis(self.sub_api_redis_key, data)
+                after_time = time.time() + self._wait_time
 
 
 class UpbitAPIProcess(BaseAPIProcess):
     pub_api_redis_key = RedisKey.ApiKey['Upbit']['publish']
     sub_api_redis_key = RedisKey.ApiKey['Upbit']['subscribe']
 
-    def __init__(self, exchange_str):
-        super(UpbitAPIProcess, self).__init__(exchange_str)
+    def __init__(self):
+        super(UpbitAPIProcess, self).__init__("Upbit")
 
 
 class BinanceAPIProcess(BaseAPIProcess):
     pub_api_redis_key = RedisKey.ApiKey['Binance']['publish']
     sub_api_redis_key = RedisKey.ApiKey['Binance']['subscribe']
 
-    def __init__(self,  exchange_str):
-        super(BinanceAPIProcess, self).__init__(exchange_str)
+    def __init__(self):
+        super(BinanceAPIProcess, self).__init__("Binance")
+
+
+if __name__ == '__main__':
+    ua = UpbitAPIProcess()
+    ua.run()
